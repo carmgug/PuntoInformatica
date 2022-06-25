751991234dd6bd99e986c7998c0560965558ee69
Carmelo Gugliotta <gglcml00h15d122u@studenti.unical.it>
2022-06-25 13:51:52 +0200

    Creazione Store Controller

--- src/main/java/it/carmelogug/puntoinformatica/controllers/rest/ProductsController.java
+++ src/main/java/it/carmelogug/puntoinformatica/controllers/rest/ProductsController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/products")
 public class ProductsController {
 
     @Autowired
---
+++ src/main/java/it/carmelogug/puntoinformatica/controllers/rest/StoreController.java
@@ -0,0 +1,13 @@
+package it.carmelogug.puntoinformatica.controllers.rest;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stores")
+public class StoreController {
+
+
+
+}
