bd3446857eda4dcd59c7dde9aebcd4301e8ad79e
Carmelo Gugliotta <gglcml00h15d122u@studenti.unical.it>
2022-06-27 00:55:50 +0200

    Definzione Purchase and ProductInPurchase

--- src/main/java/it/carmelogug/puntoinformatica/entities/ProductInPurchase.java
+++ src/main/java/it/carmelogug/puntoinformatica/entities/ProductInPurchase.java
@@ -1,5 +1,28 @@
 package it.carmelogug.puntoinformatica.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.ToString;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "ProductsInPurchase", schema = "punto_informatica")
 public class ProductInPurchase {
-    //TODO PER I PAGAMENTI
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Basic
+    @Column(name = "quantity", nullable = true)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "related_purchase")
+    @JsonIgnore
+    @ToString.Exclude
+    private Purchase purchase;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "stored_product")
+    private StoredProduct storedProduct;
 }
--- src/main/java/it/carmelogug/puntoinformatica/entities/Purchase.java
+++ src/main/java/it/carmelogug/puntoinformatica/entities/Purchase.java
@@ -1,5 +1,28 @@
 package it.carmelogug.puntoinformatica.entities;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+@Entity
+@Table(name = "Purchases", schema = "punto_informatica")
 public class Purchase {
-    //TODO PER I PAGAMENTI
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Basic
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "purchase_time")
+    private Date purchaseTime;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer")
+    private User buyer;
+
+    @OneToMany(mappedBy = "purchase", cascade = CascadeType.MERGE)
+    private List<ProductInPurchase> productsInPurchase;
 }
